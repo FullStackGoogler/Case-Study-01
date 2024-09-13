c73369c890dd310953d5e29ca24c4aeb4e8f307a
Eric Zhou <FullStackGoogler@users.noreply.huggingface.co>
2024-09-13 00:17:28 +0000

    Update app.py

--- app.py
+++ app.py
@@ -130,10 +130,8 @@ def calculate_similarities(name, data_original, data_filtered, use_local_model):
     print(f"Number of games with at least 500 recommendations: {rec_count}")
     
     # Filter games from the last 5 years
-    #five_years_before = datetime.now().year - 2
-    #games_filtered = data_original.query(f'year > {five_years_before}')
-
-    games_filtered = data_original
+    five_years_before = datetime.now().year - 10
+    games_filtered = data_original.query(f'year > {five_years_before}')
 
     result = data_filtered
 
